cc8bac201d346f2f7401e217261d3b67e597f0ea
Saikrishna <saikiven03@gmail.com>
2025-02-14 20:58:14 +0530

    Data Validation code

---
+++ networksecurity/components/data_validation.py
@@ -0,0 +1,21 @@
+from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
+from networksecurity.utils.main_utils.utils import read_yaml_file
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from scipy.stats import ks_2samp
+import pandas as pd
+import os,sys
+
+class DataValidation:
+    def __init__(self,data_ingestion_artifact:DataIngestionArtifact,
+                 data_validation_config:DataValidationConfig):
+        try:
+            self.data_ingestion_artifact=data_ingestion_artifact
+            self.data_validation_config=data_validation_config
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import numpy as np
 import pandas as pd
 
@@ -13,6 +12,7 @@ FILE_NAME:str = "phisingData.csv"
 
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 '''
 Data Ingestion related configuration start with a DATA_INGESTION VAR name
@@ -24,3 +24,15 @@ DATA_INGESTION_DIR_NAME: str ="data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+'''
+Data Validation related configuration start with a DATA_VALIDATION VAR name
+'''
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+
