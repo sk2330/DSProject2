45717972e5148e47543b8ab6310567166e19a5ab
Saikrishna <saikiven03@gmail.com>
2025-05-03 00:20:37 +0530

    Updated requirements and model training pipeline

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
 
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
